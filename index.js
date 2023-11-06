c444e56e9a68e5d27d3200f3256f419bcf9c28e9
simo75m1 <simo75m1@stud.kea.dk>
2023-11-06 14:50:54 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+
+const URL = "http://localhost:8080/api/namegen"
+
+document.getElementById("btn-get-name").addEventListener("click", getName)
+
+async function getName(){
+    const nameURL = `${URL}?description=${document.getElementById("description").value}`;
+    const spinner = document.getElementById('spinner');
+    const result = document.getElementById("result")
+    result.style.color = "black"
+
+    try {
+        spinner.style.display = "block";
+        //Add a spinner gif or? yes.
+        const response = await fetch(nameURL).then(handleHttpErrors)
+        result.innerText = response.answer;
+    } catch (error) {
+        result.style.color = "red";
+        result.innerText = error.message;
+    }
+    finally {
+        spinner.style.display = "none";
+      }
+}
+
+async function handleHttpErrors(res) {
+    if (!res.ok) {
+      const errorResponse = await res.json();
+      const msg = errorResponse.message ? errorResponse.message : "No error details provided"
+      throw new Error(msg)
+    }
+    return res.json()
+  }
